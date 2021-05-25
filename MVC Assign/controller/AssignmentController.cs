59693f2ddb550cb47e94f4704b5952eaeaad0f2b
Chetan Jadhav <63914586+chetanjadhav9981@users.noreply.github.com>
2021-05-25 20:46:44 +0530

    Add files via upload

---
+++ MVC Assign/controller/AssignmentController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Logging;
+using MVCDemoApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCDemoApp.Controllers
+{
+    public class AssignmentController : Controller
+    {
+        private readonly ILogger<AssignmentController> _logger;
+
+        public AssignmentController(ILogger<AssignmentController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult RegistrationForm()
+        {
+            return View(new Registration());
+        }
+
+        [HttpPost]
+        public ActionResult DisplayRegistrationData(Registration registration)
+
+        {
+            if(registration != null)
+            {
+                ViewBag.DefaultMessage = "Thank You" + registration.Name;
+                if (registration.MeetingStatus == "yes")
+                {
+                    ViewBag.Message = "thankyou for coming ,Please be 10 min prior to start of meeting.";
+                }
+                else if (registration.MeetingStatus == "Maybe")
+                {
+                    ViewBag.Message = "We'll Expecting You.";
+                }
+                else if(registration.MeetingStatus == "No")
+                {
+                    ViewBag.Message = "Sorry to hear tha you can't make it,but thanks for letting ud know";
+                }
+            }
+
+            return View();
+        }
+
+
+    }
+}
---
+++ MVC Assign/model/Registration.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCDemoApp.Models
+{
+    public enum MeetingStatus
+    {
+        Yes,
+        Maybe,
+        No
+    }
+    public class Registration
+    {
+       internal string MeetingStatus;
+
+        [Required(ErrorMessage = "Name is compulsary feild")]  
+        public int? Name { get; set; }
+
+        [Required(ErrorMessage = "Email name is compulsary")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Phone no is compulsary")]
+        public double? Phone { get; set; }
+
+        [Required(ErrorMessage = "Will Attend is compulsary")]
+        
+        public MeetingStatus Options { get; internal set; }
+        
+    }
+    
+}
