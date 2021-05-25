2431f23dd4ccea702af0096f8afd5d5071e6999a
Chetan Jadhav <63914586+chetanjadhav9981@users.noreply.github.com>
2021-05-25 21:13:42 +0530

    Add files via upload

---
+++ GUID.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GUIDApp
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Guid obj = Guid.NewGuid();
+            Console.WriteLine("New Guid is " + obj.ToString());
+            Console.ReadLine();
+            return -1;
+        }
+    }
+}
